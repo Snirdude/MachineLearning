6e608a18b28c4423b4e7271909078c22903c8f16
Snirdude <yacobysn@gmail.com>
2016-07-23 20:59:56 +0300

    Complete

--- Assignment1.py
+++ Assignment1.py
@@ -4,13 +4,16 @@ from matplotlib import pyplot as plt
 n = 1000
 m = 40
 
+sigmaVec = []
 xMatrix = numpy.random.rand(n, m) * 100
 bParametersVec = numpy.random.rand(m, 1) * 5
-noiseVec = numpy.random.rand(n, 1) * 100
-yVec = xMatrix.dot(bParametersVec) + noiseVec
-bEstimated = numpy.linalg.inv(xMatrix.transpose().dot(xMatrix)).dot(xMatrix.transpose().dot(yVec))
 
-differenceVec = numpy.power((bParametersVec - bEstimated), 2)
+for i in range(1, 1000):
+    noiseVec = numpy.random.rand(n, 1) * i
+    yVec = numpy.dot(xMatrix, bParametersVec) + noiseVec
+    bEstimated = numpy.dot(numpy.dot(numpy.linalg.inv(numpy.dot(xMatrix.T, xMatrix)), xMatrix.T), yVec)
+    differenceNorma = numpy.power(numpy.linalg.norm(bParametersVec - bEstimated), 2)
+    sigmaVec.append(differenceNorma)
 
-plt.plot(differenceVec)
+plt.plot(sigmaVec)
 plt.show()
