324dc08932e2e8a2501ea9539ef4cc3de3e29b2d
Snirdude <yacobysn@gmail.com>
2016-08-11 14:14:51 +0300

    Working on assignment 2.1

--- Assignment2.py
+++ Assignment2.py
@@ -1,62 +1,61 @@
 import numpy as np
 import csv
+import pandas as pd
 from matplotlib import pyplot as plt
 
 
-def computeJbeta(X, b, y, n, m, lamda):
-    squareError = 0.0
-    for t in range(0, n - 1):
-        print(type(X[t]))
-        squareError += (y[t] - b.t.dot(X[t])) ** 2
-    squareError /= 2 * n
+def computeJbeta(X, b, y, numOfExamples, numOfFeatures, lamda):
+    squareError = 0
+    for t in range(numOfExamples):
+        squareError += (y[t] - b.T.dot(X[t])) ** 2
+    squareError /= 2 * numOfExamples
     regularization = 0
-    for j in range(0, m - 1):
-        regularization += pow(beta[j], 2)
-    regularization *= lamda / (2 * m)
+    for j in range(numOfFeatures):
+        regularization += b[j] ** 2
+    regularization *= lamda / (2 * numOfFeatures)
+
     return squareError + regularization
 
 
-def gradientAscent(X, b, y, n, m, lamda):
-    squareError = 0
-    for t in range(0, n - 1):
-        squareError += (y[t] - b.transpose().dot(X[t])) * X[t]
-    squareError /= n
-    regularization = (lamda / m) * beta
-    for i in range(0, m - 1):
-        betaUpdated[i] = beta[i] + tao * (squareError + regularization)
+def gradientAscent(X, b, y, numOfExamples, numOfFeatures, i_Lamda, i_Tao):
+    betaUpdated = b.copy()
+    for j in range(numOfFeatures):
+        squareError = 0
+        for t in range(numOfExamples):
+            squareError += (y[t] - b.T.dot(X[t])) * X[t][j]
+        squareError /= numOfExamples
+        #print('squareError ' + str(squareError))
+        regularization = (i_Lamda / numOfFeatures) * b[j]
+        betaUpdated[j] = b[j] - i_Tao * (squareError + regularization)
+        print(betaUpdated[j])
 
     return betaUpdated
 
 
-X = np.ndarray
-y1 = []
-y2 = []
 J = []
-lamda = 3
-tao = 5
+lamda = 0.03
+tao = 0.01
 epsilon = 0.001
 m = 520
 beta = np.random.rand(520, 1) * 3
 betaUpdated = beta
 
-with open('trainingData.csv', 'r') as training:
-    reader = csv.reader(training, 'excel')
-    n = 0
-    for row in reader:
-        if n > 0:
-            X.put(np.ndarray(row[:520]))
-            y1.append(row[52:521])
-            y2.append(row[521:522])
-        n += 1
+X = pd.read_csv('trainingData.csv', usecols=range(0, 520))
+y1 = pd.read_csv('trainingData.csv', usecols=range(520, 521))
+y2 = pd.read_csv('trainingData.csv', usecols=range(521, 522))
+n = 500#len(y1)
 
-Jvalue = computeJbeta(X, beta, y1, n, m, lamda)
+X[X == 100] = -104
+Jvalue = computeJbeta(X.values, beta, y1.values, n, m, lamda)
 J.append(Jvalue)
-beta = gradientAscent(X, beta, y1, n, m, lamda)
-Jvalue = computeJbeta(X, beta, y1, n, m, lamda)
-while abs(Jvalue - J[len(J) - 1]) > epsilon:
+beta = gradientAscent(X.values, beta, y1.values, n, m, lamda, tao)
+Jvalue = computeJbeta(X.values, beta, y1.values, n, m, lamda)
+#while abs(Jvalue - J[len(J) - 1]) > epsilon:
+for i in range(10):
+    print(i, Jvalue)
     J.append(Jvalue)
-    beta = gradientAscent(X, beta, y1, n, m, lamda)
-    Jvalue = computeJbeta(X, beta, y1, n, m, lamda)
+    beta = gradientAscent(X.values, beta, y1.values, n, m, lamda, tao)
+    Jvalue = computeJbeta(X.values, beta, y1.values, n, m, lamda)
 
 plt.plot(J)
 plt.show()
