992c07978e7e489bed3f74075e5a5a05f13da0b8
Snirdude <yacobysn@gmail.com>
2016-08-11 19:33:39 +0300

    Working on assignment 2.2

--- Assignment2.py
+++ Assignment2.py
@@ -4,58 +4,78 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 
+# function J beta with regularization
 def computeJbeta(X, b, y, numOfExamples, numOfFeatures, lamda):
     squareError = 0
     for t in range(numOfExamples):
         squareError += (y[t] - b.T.dot(X[t])) ** 2
     squareError /= 2 * numOfExamples
-    regularization = 0
-    for j in range(numOfFeatures):
-        regularization += b[j] ** 2
+    regularization = b.T.dot(b)
     regularization *= lamda / (2 * numOfFeatures)
 
     return squareError + regularization
 
+# compute the entire derivative for J beta L1 Norm ( L2 Norm square)
+def gradientVectorL1Norm(X, b, y, numOfExamples, numOfFeatures, i_Lamda):
+    squareError = 0
+    for t in range(numOfExamples):
+        squareError += (b.T.dot(X[t]) - y[t]) * X[t]
+    squareError /= numOfExamples
+    regularization = (i_Lamda / numOfFeatures) * b
 
-def gradientAscent(X, b, y, numOfExamples, numOfFeatures, i_Lamda, i_Tao):
+    print(np.linalg.norm(squareError + regularization, 2) ** 2)
+    return np.linalg.norm(squareError + regularization, 2) ** 2
+
+
+#change vector beta simultaneously with gradient descent
+def gradientDescent(X, b, y, numOfExamples, numOfFeatures, i_Lamda, i_Tao):
     betaUpdated = b.copy()
     for j in range(numOfFeatures):
         squareError = 0
         for t in range(numOfExamples):
-            squareError += (y[t] - b.T.dot(X[t])) * X[t][j]
+            squareError += (b.T.dot(X[t]) - y[t]) * X[t][j]
         squareError /= numOfExamples
-        #print('squareError ' + str(squareError))
         regularization = (i_Lamda / numOfFeatures) * b[j]
         betaUpdated[j] = b[j] - i_Tao * (squareError + regularization)
-        print(betaUpdated[j])
 
     return betaUpdated
 
 
-J = []
-lamda = 0.03
-tao = 0.01
+J1 = []
+J2 = []
+G1 = []
+G2 = []
+lamda = 0.01
+tao = 0.000001
 epsilon = 0.001
 m = 520
-beta = np.random.rand(520, 1) * 3
-betaUpdated = beta
+n = 200
+beta1 = np.random.rand(520, 1)
+beta2 = np.random.rand(520, 1)
 
 X = pd.read_csv('trainingData.csv', usecols=range(0, 520))
 y1 = pd.read_csv('trainingData.csv', usecols=range(520, 521))
 y2 = pd.read_csv('trainingData.csv', usecols=range(521, 522))
-n = 500#len(y1)
 
-X[X == 100] = -104
-Jvalue = computeJbeta(X.values, beta, y1.values, n, m, lamda)
-J.append(Jvalue)
-beta = gradientAscent(X.values, beta, y1.values, n, m, lamda, tao)
-Jvalue = computeJbeta(X.values, beta, y1.values, n, m, lamda)
+X[X == 100] = -52 # average of signal
+Jvalue = computeJbeta(X.values, beta1, y1.values, n, m, lamda)
+J1.append(Jvalue)
+Jvalue = computeJbeta(X.values, beta2, y2.values, n, m, lamda)
+J2.append(Jvalue)
+Jvalue = computeJbeta(X.values, beta1, y1.values, n, m, lamda)
 #while abs(Jvalue - J[len(J) - 1]) > epsilon:
-for i in range(10):
-    print(i, Jvalue)
-    J.append(Jvalue)
-    beta = gradientAscent(X.values, beta, y1.values, n, m, lamda, tao)
-    Jvalue = computeJbeta(X.values, beta, y1.values, n, m, lamda)
+for i in range(20):
+    beta1 = gradientDescent(X.values, beta1, y1.values, n, m, lamda, tao)
+    beta2 = gradientDescent(X.values, beta2, y2.values, n, m, lamda, tao)
+    G1.append(gradientVectorL1Norm(X.values, beta1, y1.values, n, m, lamda))
+    G2.append(gradientVectorL1Norm(X.values, beta2, y2.values, n, m, lamda))
+    Jvalue = computeJbeta(X.values, beta1, y1.values, n, m, lamda)
+    J1.append(Jvalue[0][0])
+    Jvalue = computeJbeta(X.values, beta2, y2.values, n, m, lamda)
+    J2.append(Jvalue[0][0])
 
-plt.plot(J)
+#plt.plot(J1)
+#plt.plot(J2)
+plt.plot(G1)
+plt.plot(G2)
 plt.show()
