f22d322e5940faa7ba5f9158e83832e6b36041cf
Snirdude <yacobysn@gmail.com>
2016-09-09 15:02:54 +0300

    Working on assignment 6

--- Assignment6.py
+++ Assignment6.py
@@ -2,23 +2,38 @@ from matplotlib import pyplot as plt
 from load import mnist
 import numpy as np
 
-k_NumOfSamples = 10
+K = 10
 
 # TODO:
-def randomlyClassifyData():
-    import random
-    return random.shuffle()
+def RandomlyClassifyData(X):
+    C = list(range(10))
+    j = 0
+    for j in range(K):
+        C[j] = {'points': [], 'mean': np.ndarray(784)}
+
+    for i in range(np.size(X, 0)):
+        C[j]['points'].append(X[i])
+        j += 1
+        j %= K
+
+    for j in range(K):
+        C[j]['mean'] = np.average(C[j]['points'], axis=0)
+
+    return C
+
 
 # TODO:
 def Kmeans(i_Data, i_K):
-    centroids = randomlyClassifyData(i_K)
+    centroids = RandomlyClassifyData(i_K)
     costFunctionValues = []
 
     return centroids, costFunctionValues
 
 # TODO:
-def CostFunction():
-    return 0
+def CostFunction(C):
+    pass
 
-#trX, teX, trY, teY = mnist(ntrain=60000, ntest=10000, onehot=True)
+trX, teX, trY, teY = mnist(ntrain=60000, ntest=10000, onehot=True)
+C = RandomlyClassifyData(trX)
+print(C)
 #centroids, costFunctionValues = Kmeans(trX, k_NumOfSamples)
