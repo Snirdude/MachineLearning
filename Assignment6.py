15ca492c812a773eb5bdc97225c2d163b7d99959
Snirdude <yacobysn@gmail.com>
2016-09-10 14:10:18 +0300

    Working on assignment 6

--- Assignment6.py
+++ Assignment6.py
@@ -3,10 +3,41 @@ from load import mnist
 import numpy as np
 from scipy.spatial import distance
 from collections import Counter
-from sklearn.cluster import KMeans
 K = 10
 
+def RandomlyClassifyDataBestMatch(X):
+    """
+    initialize the centroids with known images for best centroid start
+    :param X: the train data matrix
+    :return: list of dictionaries with centroids location and all the images inside
+    """
+    C = list(range(10))
+
+    for j in range(K):
+        C[j] = {'points': [], 'mean': np.ndarray(784)}
+
+    C[0]['points'].append(X[1].tolist())
+    C[1]['points'].append(X[3].tolist())
+    C[2]['points'].append(X[5].tolist())
+    C[3]['points'].append(X[7].tolist())
+    C[4]['points'].append(X[2].tolist())
+    C[5]['points'].append(X[0].tolist())
+    C[6]['points'].append(X[13].tolist())
+    C[7]['points'].append(X[15].tolist())
+    C[8]['points'].append(X[17].tolist())
+    C[9]['points'].append(X[116].tolist())
+
+    for j in range(K):
+        C[j]['mean'] = np.average(C[j]['points'], axis=0)
+
+    return C
+
 def RandomlyClassifyData(X):
+    """
+    initialize the centroids kind of random data
+    :param X: the train data matrix
+    :return: list of dictionaries with centroids location and all the images inside
+    """
     C = list(range(10))
 
     for j in range(K):
@@ -35,7 +66,8 @@ def findGivenVectorInAllCentroid(C, vectorToLookFor):
 
 def Kmeans(X):
     k_MaxAllowdExchanges = int(np.size(X, 0) * 0.02)
-    C = RandomlyClassifyData(X)
+    C = RandomlyClassifyDataBestMatch(X)    # for best initialization
+    # C = RandomlyClassifyData(X)           # for random initialization
     costFunctionValues = []
     numOfExchanges = np.inf
 
@@ -49,14 +81,13 @@ def Kmeans(X):
                 distances.append(distance.euclidean(X[i], C[j]['mean']))
 
             rightCentroidNumber = np.argmin(distances)
-            wrongCentroidNumber = findGivenVectorInAllCentroid(C, X[i].tolist())
 
+            wrongCentroidNumber = findGivenVectorInAllCentroid(C, X[i].tolist())
             if rightCentroidNumber != wrongCentroidNumber:
                 numOfExchanges += 1
-                C[wrongCentroidNumber]['points'].remove(X[i].tolist())
+                if wrongCentroidNumber != -1:
+                    C[wrongCentroidNumber]['points'].remove(X[i].tolist())
                 C[rightCentroidNumber]['points'].append(X[i].tolist())
-
-            distances.clear()
 
         # rearrange the centroids
         for j in range(K):
@@ -93,22 +124,13 @@ def successRate(C, X, Y):
         correctOccurences += numOfOccurences
         falseOccurences += len(indices)
 
-    return (falseOccurences / (falseOccurences + correctOccurences)) * 100
+    return (correctOccurences / (falseOccurences + correctOccurences)) * 100
 
 trX, teX, trY, teY = mnist(ntrain=60000, ntest=10000, onehot=False)
-X = trX[0:1000]
-Y = trY[0:1000]
-# C, CostFunctionValues = Kmeans(X)
-# print(successRate(C, X, Y))
-# plt.plot(CostFunctionValues)
-# plt.show()
-# for j in range(K):
-#     plt.imshow(C[j]['mean'].reshape((28, 28)))
-#     plt.show()
+X = trX[0:200]
+Y = trY[0:200]
+C, CostFunctionValues = Kmeans(X)
 
-kmeans = KMeans(n_clusters=10)
-kmeans.fit(X)
-
-kmeansLabelDict = {}
-for element in kmeans.labels_:
-    kmeans
+print(successRate(C, X, Y))
+plt.plot(CostFunctionValues)
+plt.show()
